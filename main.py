d73bf068780b8386f70c376a25bab6e049db013c
Boris Shishov <borisshishov@gmail.com>
2019-12-24 03:47:13 +0300

    Corners without fitting

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import numpy as np
 from scipy.optimize import differential_evolution, basinhopping, minimize
 
 from pieces_utils import gen_piece, gen_piece_lite, gen_piece_options, get_options_number, NUM_PARAMS, NUM_PARAMS_LITE, NUM_PARAMS_CORNERS
-from utils import arrange4points, benchmark
+from utils import benchmark, center_contour, iter_curve_sides, resample_curve, curve_corners
 from cv_tools import MeanFrameOverTime, ColoredMarkerDetector, InversePerspective, PieceDetector
 
 # ['size', 'rotation', 'neck_heights', 'corner_offset', 'middle_offset', 'radius', 'ellipse']
-OPTIONS = ['size', 'rotation', 'neck_heights']
-VIS = 0
+OPTIONS = ['size', 'rotation']
+
+VIS_SHAPE_FIT = False
+VIS_LITE_SHAPE_FIT = False
+VIS_OPTIONS_FIT = False
+
 
 def contour_at_canvas_center(contour, canvas_size=(256, 256), dst=None):
     """ Draws contour at a center of a canvas """
@@ -22,37 +26,6 @@ def contour_at_canvas_center(contour, canvas_size=(256, 256), dst=None):
 
     cv.fillPoly(dst, pts=[np.int0(contour)], color=(255, 255, 255), lineType=cv.LINE_AA)
     return dst
-
-
-def center_contour(contour):
-    contour = np.float32(contour)
-    return contour - np.squeeze(np.mean(contour, axis=0))
-
-
-def find_nearest(curve, point):
-    distances = np.squeeze(np.sum((curve - point) ** 2, axis=-1))
-    return np.argsort(distances)[0]
-
-
-def iter_sides(contour, corners):
-    corner_indexes = sorted(find_nearest(contour, corner) for corner in corners)
-    yield np.concatenate((contour[corner_indexes[-1]:], contour[:corner_indexes[0] + 1]))
-    start = corner_indexes[0]
-    for end in corner_indexes[1:]:
-        yield contour[start:end+1]
-        start = end
-
-
-def resample(curve, samples=20):
-    curve = np.asarray(curve)
-
-    space_old = np.linspace(0, 1, curve.shape[0])
-    space_new = np.linspace(0, 1, samples)
-
-    res = np.zeros((samples, 2), dtype=np.float32)
-    res[:, 0] = np.interp(space_new, space_old, curve[:, 0])
-    res[:, 1] = np.interp(space_new, space_old, curve[:, 1])
-    return res
 
 
 class Optimization:
@@ -70,30 +43,37 @@ class Optimization:
     def minimize(self, args) -> float:
         polygon, _ = gen_piece(args)
         sample = contour_at_canvas_center(polygon)
-        cv.imshow('align1', sample)
-        cv.waitKey(1)
+        if VIS_SHAPE_FIT:
+            cv.imshow('fit', sample)
+            cv.waitKey(1)
 
         return self.distance(self.normalize(sample), self.target)
 
     def minimize_lite(self, args) -> float:
         polygon, _ = gen_piece_lite(args)
         sample = contour_at_canvas_center(polygon)
-        # cv.imshow('align1', sample)
-        # cv.waitKey(1)
+        if VIS_SHAPE_FIT:
+            cv.imshow('shape-fit', sample)
+            cv.waitKey(1)
 
         return self.distance(self.normalize(sample), self.target)
 
     def minimize_options(self, args) -> float:
         polygon, _ = gen_piece_options(args, OPTIONS)
         sample = contour_at_canvas_center(polygon)
-        if VIS:
-            cv.imshow('align1', sample)
+        if VIS_OPTIONS_FIT:
+            cv.imshow('options-fit', sample)
             cv.waitKey(1)
 
         return self.distance(self.normalize(sample), self.target)
 
 
-def find_options(piece_contour):
+def find_contour_corners(piece_contour):
+    piece_contour = np.squeeze(np.asarray(piece_contour, dtype=np.float32))
+    return curve_corners(piece_contour)
+
+
+def find_contour_corners_opt(piece_contour):
     piece_contour = np.squeeze(np.asarray(piece_contour, dtype=np.float32))
     piece_center = np.squeeze(np.mean(piece_contour, axis=0))
 
@@ -104,7 +84,7 @@ def find_options(piece_contour):
     with benchmark('Evolution'):
         opt_res = differential_evolution(opt.minimize_options,
                                          bounds=[(-1, 1)] * get_options_number(OPTIONS),
-                                         tol=0.08)
+                                         tol=0.05)
 
     generated, corners = gen_piece_options(opt_res.x, OPTIONS)
     generated_offset = np.squeeze(np.mean(generated, axis=0))
@@ -112,16 +92,20 @@ def find_options(piece_contour):
 
 
 def find_piece_sides(piece_contour, debug=False):
-    piece_contour = np.squeeze(np.asarray(piece_contour, dtype=np.float32))
-    corners = find_options(piece_contour)
-    sides = list(map(resample, iter_sides(piece_contour, corners)))
+    piece_contour = np.squeeze(np.asarray(piece_contour, dtype=np.float32)) * 6
+    with benchmark('Corners'):
+        corners = find_contour_corners(piece_contour)
+    sides = list(map(resample_curve, iter_curve_sides(piece_contour, corners)))
+    #sides = list(iter_curve_sides(piece_contour, corners))
 
     if debug:
+        w, h = 1024, 1024
+        canvas_center = np.float32((w, h)) * 0.5
         piece_center = np.squeeze(np.mean(piece_contour, axis=0))
-        dst = np.zeros((512, 512, 3), np.uint8)
+        dst = np.zeros((w, h, 3), np.uint8)
 
         for corner in corners:
-            cv.drawMarker(dst, tuple(np.int0(corner + np.float32([256, 256] - piece_center))), (255, 0, 0))
+            cv.drawMarker(dst, tuple(np.int0(corner + canvas_center - piece_center)), (255, 0, 0))
 
         colors = [
             (255, 255, 255),
@@ -132,10 +116,11 @@ def find_piece_sides(piece_contour, debug=False):
         for side, color in zip(sides, colors):
             if side.shape[0] > 2:
                 cv.polylines(dst,
-                             [np.int0(resample(side) + np.float32([256, 256] - piece_center))],
+                             #[np.int0(resample_curve(side) + np.float32([512, 512] - piece_center))],
+                             [np.int0(side + canvas_center - piece_center)],
                              isClosed=False,
                              color=color,
-                             thickness=2)
+                             thickness=1)
 
         cv.namedWindow('find_piece_sides')
         cv.imshow('find_piece_sides', dst)
@@ -153,11 +138,11 @@ def piece_sides_test(filename='sample_piece.png'):
     if cv.__version__ == '3.4.4':
         _, contours, _ = cv.findContours(sample_in_range,
                                          mode=cv.RETR_EXTERNAL,
-                                         method=cv.CHAIN_APPROX_SIMPLE)
+                                         method=cv.CHAIN_APPROX_TC89_KCOS)
     else:
-        contours, hierarchy = cv.findContours(sample_in_range,
-                                              mode=cv.RETR_EXTERNAL,
-                                              method=cv.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv.findContours(sample_in_range,
+                                      mode=cv.RETR_EXTERNAL,
+                                      method=cv.CHAIN_APPROX_TC89_KCOS)
     target_contour = contours[0]
     cv.imshow('Piece', contour_at_canvas_center(target_contour))
     cv.waitKey(1)
--- utils.py
+++ utils.py
@@ -39,3 +39,87 @@ def benchmark(name: str):
         end = time.time()
         print(f'Finished: {name} time: {end - start}')
 
+
+def center_contour(contour):
+    contour = np.float32(contour)
+    return contour - np.squeeze(np.mean(contour, axis=0))
+
+
+def index_of_nearest_to_point(curve, point):
+    distances = np.squeeze(np.sqrt(np.sum((curve - point) ** 2, axis=-1)))
+    return np.argmin(distances)
+
+
+def iter_curve_sides(contour, corners):
+    corner_indexes = sorted(index_of_nearest_to_point(contour, corner) for corner in corners)
+    yield np.concatenate((contour[corner_indexes[-1]:], contour[:corner_indexes[0] + 1]))
+    start = corner_indexes[0]
+    for end in corner_indexes[1:]:
+        yield contour[start:end+1]
+        start = end
+
+
+def resample_curve(curve, samples=20):
+    curve = np.asarray(curve)
+
+    space_old = np.linspace(0, 1, curve.shape[0])
+    space_new = np.linspace(0, 1, samples)
+
+    res = np.zeros((samples, 2), dtype=np.float32)
+    res[:, 0] = np.interp(space_new, space_old, curve[:, 0])
+    res[:, 1] = np.interp(space_new, space_old, curve[:, 1])
+    return res
+
+
+def angle3(a, b, c):
+    """ Find angle between a-b and b-c """
+    angle = np.arctan2(c[1] - b[1], c[0] - b[0]) - \
+            np.arctan2(a[1] - b[1], a[0] - b[0])
+    if angle < np.pi:
+        angle += 2 * np.pi
+    if angle > np.pi:
+        angle -= 2 * np.pi
+    return angle
+
+
+def calc_angles(curve):
+    n = len(curve)
+    yield angle3(curve[-1], curve[0], curve[1])
+    for i in range(1, n-1):
+        yield angle3(curve[i-1], curve[i], curve[i+1])
+    yield angle3(curve[-2], curve[-1], curve[0])
+
+
+def curve_corners(curve, num_corners=4, corner_threshold=0.7 * np.pi):
+    angles = np.abs(list(calc_angles(curve)))
+    curve = curve[angles < corner_threshold]
+
+    center = np.squeeze(np.mean(curve, axis=0))
+    distances = np.squeeze(np.sqrt(np.sum((curve - center) ** 2, axis=-1)))
+
+    indices = np.argsort(distances)[-num_corners:]
+    return curve[indices]
+
+
+def test():
+    print(angle3(
+        np.array([0, 0]),
+        np.array([0, 1]),
+        np.array([0, 0]),
+    ))
+
+    curve = [
+        (0, 0),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+    ]
+    curve = np.asarray(curve)
+    print(list(calc_angles(curve)))
+
+    reversed_curve = np.asarray(list(reversed(curve)))
+    print(list(calc_angles(reversed_curve)))
+
+
+if __name__ == '__main__':
+   test()
